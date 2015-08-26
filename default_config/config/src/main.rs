22c332b6646e5a9efee09d51d16f7646008f0606
Ryan Rice <ryanr1230@gmail.com>
2015-08-26 00:28:33 -0500

    making generated app have no warnings

--- default_config/config/src/main.rs
+++ default_config/config/src/main.rs
@@ -1,8 +1,7 @@
 extern crate bolts;
 
-use std::path::Path;
 include!("../config.rs");
 fn main() {
 
-	bolts::run(config::default_layout(), config::markdown_file());
+	bolts::run(config::default_layout(), config::markdown_file()).unwrap();
 }
